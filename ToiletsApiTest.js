9b7ff4c46333cdcc570a3df404ab2569fb791d04
Sjoerd Visscher <sjoerd@q42.nl>
2015-03-13 13:28:03 +0100

    testing actual machine

--- ToiletsApiTest.js
+++ ToiletsApiTest.js
@@ -12,11 +12,19 @@ if (Meteor.isClient) {
 }
 
 if (Meteor.isServer) {
+  var ToiletsServer
   Meteor.startup(function () {
-    var ToiletsServer = DDP.connect("localhost:3000");
+    ToiletsServer = DDP.connect("wclights.q070.nl:3000");
     Toilets = new Mongo.Collection("toilets", ToiletsServer)
     ToiletsServer.subscribe("toilets");
   });
+  
+  Meteor.methods({
+    status: function() {
+      console.log(ToiletsServer.status());
+      return ToiletsServer.status();
+    }
+  })
 
   Meteor.publish("toilets", function() {
     return Toilets.find({});
