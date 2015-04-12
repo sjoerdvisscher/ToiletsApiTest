a644a9f5c334dc86282995939b472936f6278935
Sjoerd Visscher <sjoerd@w3future.com>
2015-04-12 19:34:23 +0200

    Show q070 lift

--- ToiletsApiTest.js
+++ ToiletsApiTest.js
@@ -1,27 +1,40 @@
 var Toilets
+var Lifts
 
 if (Meteor.isClient) {
   Toilets = new Mongo.Collection("toilets");
   Meteor.subscribe("toilets");
 
+  Lifts = new Mongo.Collection("lifts");
+  Meteor.subscribe("lifts");
+
   Template.hello.helpers({
     toilet: function () {
       return Toilets.find({})
+    },
+    lift: function () {
+      return Lifts.find({})
     }
   });
 }
 
 if (Meteor.isServer) {
   var ToiletsServer
+  var LiftsServer
   Meteor.startup(function () {
     ToiletsServer = DDP.connect("wclights.q070.nl");
     Toilets = new Mongo.Collection("toilets", ToiletsServer)
     ToiletsServer.subscribe("toilets");
+
+    LiftsServer = DDP.connect("10.42.35.50");
+    Lifts = new Mongo.Collection("lifts", LiftsServer);
+    LiftsServer.subscribe("lifts");
   });
-  
+
   Meteor.methods({
     status: function() {
       console.log(ToiletsServer.status());
+      console.log(LiftsServer.status());
       return ToiletsServer.status();
     }
   })
@@ -29,4 +42,9 @@ if (Meteor.isServer) {
   Meteor.publish("toilets", function() {
     return Toilets.find({});
   })
+  Meteor.publish("lifts", function() {
+    return Lifts.find({});
+  })
+
+
 }
