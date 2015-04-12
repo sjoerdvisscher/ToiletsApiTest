f378da8454bc540531a30b95d2dbd4ca86024378
Sjoerd Visscher <sjoerd@w3future.com>
2015-04-12 19:51:22 +0200

    use public server

--- ToiletsApiTest.js
+++ ToiletsApiTest.js
@@ -1,40 +1,35 @@
 var Toilets
-var Lifts
+var Elevators
 
 if (Meteor.isClient) {
   Toilets = new Mongo.Collection("toilets");
   Meteor.subscribe("toilets");
 
-  Lifts = new Mongo.Collection("lifts");
-  Meteor.subscribe("lifts");
+  Elevators = new Mongo.Collection("elevators");
+  Meteor.subscribe("elevators");
 
   Template.hello.helpers({
     toilet: function () {
       return Toilets.find({})
     },
-    lift: function () {
-      return Lifts.find({})
+    elevator: function () {
+      return Elevators.find({})
     }
   });
 }
 
 if (Meteor.isServer) {
   var ToiletsServer
-  var LiftsServer
   Meteor.startup(function () {
     ToiletsServer = DDP.connect("wclights.q070.nl");
     Toilets = new Mongo.Collection("toilets", ToiletsServer)
     ToiletsServer.subscribe("toilets");
-
-    LiftsServer = DDP.connect("10.42.35.50");
-    Lifts = new Mongo.Collection("lifts", LiftsServer);
-    LiftsServer.subscribe("lifts");
+    ToiletsServer.subscribe("elevators");
   });
 
   Meteor.methods({
     status: function() {
       console.log(ToiletsServer.status());
-      console.log(LiftsServer.status());
       return ToiletsServer.status();
     }
   })
@@ -42,8 +37,8 @@ if (Meteor.isServer) {
   Meteor.publish("toilets", function() {
     return Toilets.find({});
   })
-  Meteor.publish("lifts", function() {
-    return Lifts.find({});
+  Meteor.publish("elevators", function() {
+    return Elevators.find({});
   })
 
 
